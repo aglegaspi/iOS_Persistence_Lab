46cab76c1fd283d2850d55739df08f6d9d97b444
aglegaspi <lex83nyc@gmail.com>
2019-09-30 15:47:08 -0400

    set up controllers and views

---
+++ iOS_Persistence_Lab/Controllers/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  iOS_Persistence_Lab
+//
+//  Created by Alexander George Legaspi on 9/30/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS_Persistence_Lab/Views/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  iOS_Persistence_Lab
+//
+//  Created by Alexander George Legaspi on 9/30/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
